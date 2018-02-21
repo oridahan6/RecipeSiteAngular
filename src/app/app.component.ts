09fc19a439e8c3e7f3d74751fb082aa4a83e8d80
Ori Dahan <ori.dahan6@gmail.com>
2018-02-21 14:10:01 +0200

    import the data service in the app component and draw all the recipes title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { DataService } from './data.service';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+	// Define a users property to hold our user data
+	recipes: Array<any>;
+
+	// Create an instance of the DataService through dependency injection
+	constructor(private _dataService: DataService) {
+
+	// Access the Data Service's getRecipes() method we defined
+	this._dataService.getRecipes()
+	    .subscribe(res => this.recipes = res);
+	}
 }
