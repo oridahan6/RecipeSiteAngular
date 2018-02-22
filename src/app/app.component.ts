e9bc34cb2deeb67faba5432a51df3c1015e6729d
Ori Dahan <ori.dahan6@gmail.com>
2018-02-22 12:55:39 +0200

    moved recipe list from main app component to recipe list compnent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 
-import { DataService } from './services/data.service';
-
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	// Define a users property to hold our user data
-	recipes: Array<any>;
 
-	// Create an instance of the DataService through dependency injection
-	constructor(private _dataService: DataService) {
-
-	// Access the Data Service's getRecipes() method we defined
-	this._dataService.getRecipes()
-	    .subscribe(res => this.recipes = res);
-	}
 }
--- src/app/recipes-list/recipes-list.component.ts
+++ src/app/recipes-list/recipes-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../services/data.service';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+	// Define a users property to hold our user data
+	recipes: Array<any>;
 
-  constructor() { }
+	ngOnInit() {
+	}
 
-  ngOnInit() {
-  }
+	// Create an instance of the DataService through dependency injection
+	constructor(private _dataService: DataService) {
 
+		// Access the Data Service's getRecipes() method we defined
+		this._dataService.getRecipes()
+		    .subscribe(res => this.recipes = res);
+	}
 }
