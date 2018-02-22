3ba28b28e07fa95c2e1f2546c67e39b609ac764b
Ori Dahan <ori.dahan6@gmail.com>
2018-02-22 09:04:58 +0200

    added ng auto serve

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // API location
 app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 //Set Port
@@ -29,3 +29,7 @@ app.set('port', port);
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+
+// add a route to add recipes
+// decide whether the category id "catid" should be removed and use the "_id" field instead in the recipe document
