8d9e8d1092fe41af37aed09f0812aa99158fd6ce
Ori Dahan <ori.dahan6@gmail.com>
2018-03-06 15:47:58 +0200

    updated service to get categories, units and cuisines

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 
 import { Http, Headers, RequestOptions } from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import { Category } from '../model/category';
+import { Unit } from '../model/unit';
+import { Cuisine } from '../model/cuisine';
 
 @Injectable()
 export class DataService {
@@ -10,9 +15,28 @@ export class DataService {
 
 	constructor(private _http: Http) { }
 
+	// add Observable<Recipe[]>
 	getRecipes() {
 		return this._http.get("/api/recipes")
 	  		.map(result => this.result = result.json().data);
+	}
+
+	getCategories() : Observable<Category[]> {
+		return this._http.get("/api/categories")
+	  		.map(result => result.json())
+	  		.map(res => <Category[]>res.categories);;
+	}
+
+	getUnits() : Observable<Unit[]> {
+		return this._http.get("/api/units")
+	  		.map(result => result.json())
+	  		.map(res => <Unit[]>res.units);;
+	}
+
+	getCuisines() : Observable<Cuisine[]> {
+		return this._http.get("/api/cuisines")
+	  		.map(result => result.json())
+	  		.map(res => <Cuisine[]>res.cuisines);;
 	}
 
 }
