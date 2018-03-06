961a776e1c2c7d880691abab3efc850074e80bb7
Ori Dahan <ori.dahan6@gmail.com>
2018-03-06 11:22:11 +0200

    added todo

--- src/app/model/recipe.ts
+++ src/app/model/recipe.ts
@@ -2,6 +2,9 @@ import { Ingredient } from '../model/ingredient';
 import { Direction } from '../model/direction';
 import { Category } from '../model/category';
 
+// TODO: decide whether to save the while categories, cuisines, mainIngredients objects instead of only their id 
+// (based on how often they change - https://www.mongodb.com/blog/post/6-rules-of-thumb-for-mongodb-schema-design-part-1)
+
 export class Recipe {
 	constructor(
 		public id: number,	
